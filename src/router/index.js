65ae87c898f95a3815c292e6af1ba0fe16d51c3e
Muhammad Rayyan <muhammad.abhad@btpn.com>
2021-01-01 00:32:16 +0700

    add bottom navigator

--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,14 @@ import {
   Hospitals,
 } from '../pages';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {BottomNavigator} from '../components';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const MainApp = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator tabBar={(props) => <BottomNavigator {...props} />}>
       <Tab.Screen name="Doctor" component={Doctor} />
       <Tab.Screen name="Messages" component={Messages} />
       <Tab.Screen name="Hospitals" component={Hospitals} />
