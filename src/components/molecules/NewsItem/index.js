18fa523a2dda6ba2ea4d4875fbb1f7d40cbd013b
Muhammad Rayyan <muhammad.abhad@btpn.com>
2021-01-01 14:19:54 +0700

    create page Doctor

---
+++ src/components/molecules/NewsItem/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import {DummyNews} from '../../../assets';
+import {colors, fonts} from '../../../utils';
+
+const NewsItem = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleWrapper}>
+        <Text style={styles.title}>Happy New Year</Text>
+        <Text style={styles.date}>Today</Text>
+      </View>
+      <Image source={DummyNews} style={styles.image} />
+    </View>
+  );
+};
+
+export default NewsItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.border,
+    paddingBottom: 12,
+    paddingTop: 16,
+    paddingHorizontal: 16,
+  },
+  titleWrapper: {flex: 1},
+  title: {
+    fontSize: 16,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    maxWidth: '90%',
+  },
+  date: {
+    fontSize: 12,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.secondary,
+  },
+  image: {
+    width: 80,
+    height: 60,
+    borderRadius: 11,
+  },
+});
