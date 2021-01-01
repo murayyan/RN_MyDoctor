4b4f408a000597b23d94cfb6119c29a405b30646
Muhammad Rayyan <muhammad.abhad@btpn.com>
2021-01-01 14:17:25 +0700

    add assets

---
+++ src/assets/dummy/index.js
@@ -0,0 +1,6 @@
+import DummyUser from './user.png';
+import DummyNews from './news.png';
+import DummyDoctor from './doctor.png';
+import DummyHospital from './hospital.png';
+
+export {DummyUser, DummyNews, DummyDoctor, DummyHospital};
